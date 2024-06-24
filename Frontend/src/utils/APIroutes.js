9a8ecab31ff9b64bbdd6c9f9af8594b1c5a49d59
Kunal Agarwal <kunalagarwal9204@gmail.com>
2024-06-24 20:53:30 +0530

    changing base-url

--- Frontend/src/utils/APIroutes.js
+++ Frontend/src/utils/APIroutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://real-time-chatapp-2lpz.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
@@ -6,4 +6,3 @@ export const allUsersRoute = `${host}/api/auth/allusers`;
 export const sendMessageRoute = `${host}/api/message/addmsg`;
 export const recieveMessageRoute = `${host}/api/message/getmsg`;
 export const setAvatarRoute = `${host}/api/auth/setavatar`;
-
